34f85aff2b855ffc8b8cac828531e5dd6df73929
paulo escobar <paulo.escobar@globant.com>
2019-05-23 22:16:09 -0500

    charts para platos mas vendidos

--- build/generated-sources/ap-source-output/entidades/DetalleFactura_.java
+++ build/generated-sources/ap-source-output/entidades/DetalleFactura_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-23T21:44:40")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-05-23T22:11:39")
 @StaticMetamodel(DetalleFactura.class)
 public class DetalleFactura_ { 
 
